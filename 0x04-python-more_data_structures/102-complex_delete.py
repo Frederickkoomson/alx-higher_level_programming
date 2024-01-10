16fd4fa762ee371b4ca227e86a99cd740cd927ee
frederickkoomson <frederickkoomson25@gmail.com>
2024-01-10 05:31:45 -0800

    some added files

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-
-
-def complex_delete(my_dict, value):
-    if my_dict:
-        to_del = []
-        for k, v in my_dict.items():
-            if v is value:
-                to_del.append(k)
-        for k in to_del:
-            del(my_dict[k])
-    return my_dict
+def complex_delete(a_dictionary, value):
+    for i in a_dictionary.copy():
+        if a_dictionary[i] is value:
+            a_dictionary.pop(i)
+    return a_dictionary
